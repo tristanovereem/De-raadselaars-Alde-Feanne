934029acbcb980e47d22c43e21b428b5a51c18d5
levi <levi.kingma@gmail.com>
2025-02-08 17:21:30 +0100

    Lijst aan taken toegevoegd

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # TODO: Zet applicatie op om web applicatie te worden, het is nu alleen een website
+# TODO: Maak QR Scan process lokaal
+# TODO: Maak unique users
+# TODO: Maak punten systeem voor goede antwoorden
+# TODO: Voeg echte inhoud toe (vragen, ui, etc.)
+# TODO: Maak kaart voor alle bord locaties
+
+# TODO (optioneel): Maak systeem om via een UI bord locaties/vragen toe te voegen, misschien met een QR Code generator erbij (Zodat dit in meerdere locaties toegepast kan worden)
+# TODO (optioneel): Room systeem, groep van 4-6 kan dan via een code in een room gaan en tegen elkaar gaan door middel van score systeem
+
+# NOTE: Beargumenteer in de documentatie waarom/of flask de beste keuze is
 
 # Root, laat camera zien
 @app.route('/')
@@ -10,9 +20,10 @@ def index():
 
 # Laat test vraag zien
 @app.route('/test-vraag')
-def new_directory():
+def test_vraag():
     return render_template("vraag.html")
 
 # Laad app in op netwerk en in debug mode
+# host='0.0.0.0' maakt de app toegangkelijk voor iedereen op het netwerk, gebruik niet in openbare ruimtes
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
