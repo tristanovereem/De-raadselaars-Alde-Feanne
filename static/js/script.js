b3059c89a867899ae1fe5697a74c9a10fffbf755
Piet <piethboterhoek@gmail.com>
2025-02-25 11:03:07 +0100

    een prompt gemaakt om terug naar de qr code scanner te gaan + alles gecentreerd

--- static/js/script.js
+++ static/js/script.js
@@ -89,11 +89,17 @@ startCamera();
 // Check of een antwoord de juiste argument heeft
 function checkAnswer(answer) {
     const resultDiv = document.getElementById('result');
+    const resultCon = document.getElementsByClassName('resultContainer')[0];
+    const main = document.getElementsByClassName('main')[0];
     if (answer === 'correct') {
+        main.style.display = "none";
+        resultCon.style.display = "flex"
         resultDiv.textContent = 'Correct!';
         resultDiv.style.color = 'green';
     } else {
-        resultDiv.textContent = 'Fout, probeer opnieuw';
+        main.style.display = "none";
+        resultCon.style.display = "flex"
+        resultDiv.textContent = 'Helees, dit was niet het juiste antwoord.';
         resultDiv.style.color = 'red';
     }
 }
