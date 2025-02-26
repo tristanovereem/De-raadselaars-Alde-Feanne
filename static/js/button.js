e67389d950468f09c9ee7168fc9c679161207cb1
levi <levi.kingma@gmail.com>
2025-02-26 20:36:33 +0100

    Score toegevoegd als snelle proof of concept

--- static/js/button.js
+++ static/js/button.js
@@ -3,14 +3,19 @@ function checkAnswer(answer) {
     const resultDiv = document.getElementById('result');
     const resultCon = document.getElementsByClassName('resultContainer')[0];
     const main = document.getElementsByClassName('main')[0];
+    
+    let counter = localStorage.getItem('counter') ? parseInt(localStorage.getItem('counter')) : 0;
+
     if (answer === 'correct') {
         main.style.display = "none";
-        resultCon.style.display = "flex"
+        resultCon.style.display = "flex";
+        counter += 1;
+        localStorage.setItem('counter', counter);
         resultDiv.textContent = 'Correct!';
         resultDiv.style.color = 'green';
     } else {
         main.style.display = "none";
-        resultCon.style.display = "flex"
+        resultCon.style.display = "flex";
         resultDiv.textContent = 'Helaas, dit was niet het juiste antwoord.';
         resultDiv.style.color = 'red';
     }
